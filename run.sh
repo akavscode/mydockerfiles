cfa02dc283058f3a4d47e7d26a86d3f11a49c48e
aka <66773152+akavscode@users.noreply.github.com>
2020-10-11 23:05:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+rm /etc/apt/sources.list
+echo 'deb https://mirrors.ustc.edu.cn/ubuntu focal main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb https://mirrors.ustc.edu.cn/ubuntu focal-backports main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb https://mirrors.ustc.edu.cn/ubuntu focal-proposed main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb https://mirrors.ustc.edu.cn/ubuntu focal-security main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb https://mirrors.ustc.edu.cn/ubuntu focal-updates main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb-src https://mirrors.ustc.edu.cn/ubuntu focal main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb-src https://mirrors.ustc.edu.cn/ubuntu focal-backports main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb-src https://mirrors.ustc.edu.cn/ubuntu focal-proposed main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb-src https://mirrors.ustc.edu.cn/ubuntu focal-security main restricted universe multiverse'>>/etc/apt/sources.list
+echo 'deb-src https://mirrors.ustc.edu.cn/ubuntu focal-updates main restricted universe multiverse'>>/etc/apt/sources.list
+
 if [ "${KEY}" != "**None**" ]; then
     echo "=> Found authorized keys"
     mkdir -p /root/.ssh
